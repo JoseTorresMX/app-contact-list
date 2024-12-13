9058134669df332bcc8f9ea5df2d8bc919911fa5
JoseTorres <joseangel.tosa@outlook.com>
2024-12-12 17:33:54 -0700

    app contactos ok,

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Agenda de contactos web</title>
+	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<?php require_once "dependencias.php"; ?>
+</head>
+
+<body>
+	<div class="container">
+		<?php require_once "menu.php"; ?>
+
+		<div class="jumbotron">
+			<h1 class="display-4">Agenda de contactos</h1>
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="card">
+						
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<table class="table table-hover">
+								<tr>
+									<th>
+										Agenda desarrollada para la materia: Desarrollo de Aplicaciones Web Reactivas
+									</th>
+								</tr>
+								<tr>
+									<th>
+										Desarrollado por Jose Torres
+									</th>
+								</tr>
+								<tr>
+									<th>
+										#Control: 19112115
+									</th>
+								</tr>
+								<tr>
+									<th>
+										Hecho con php y mysql
+									</th>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<hr class="my-4">
+
+		</div>
+
+	</div>
+
+
+</body>
+
+</html>
